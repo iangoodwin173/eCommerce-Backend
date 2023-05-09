5f970b609990a472cf996eac3b4ba05098bdccbf
Ian Goodwin <iangoodwin173@gmail.com>
2023-05-08 21:03:23 -0400

    many changes regarding routes and SQL

--- models/Product.js
+++ models/Product.js
@@ -12,12 +12,16 @@ Product.init(
     // defining columns
     id: {
       type: DataTypes.INTEGER,
-      allowNull: false,
       primaryKey: true,
+      allowNull: false,
       autoIncrement: true,
     },
     product_name: {
-      type: DataTypes.DECIMAL(10,2),
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
       allowNull: false,
       validate: {
         isDecimal: true
